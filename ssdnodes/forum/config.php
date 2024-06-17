6f08e53e830d5eb09cc6540e5ee23daab2ff7351
MetroWind <chris.corsair@gmail.com>
2024-06-17 10:51:36 -0700

    Make the forum playbook repeatable.

---
+++ ssdnodes/forum/config.php
@@ -0,0 +1,16 @@
+<?php
+// phpBB 3.3.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'phpbb\\db\\driver\\sqlite3';
+$dbhost = '/var/lib/phpbb/db';
+$dbport = '';
+$dbname = '';
+$dbuser = '';
+$dbpasswd = '';
+$table_prefix = 'phpbb_';
+$phpbb_adm_relative_path = 'adm/';
+$acm_type = 'phpbb\\cache\\driver\\file';
+
+@define('PHPBB_INSTALLED', true);
+@define('PHPBB_ENVIRONMENT', 'production');
+// @define('DEBUG_CONTAINER', true);
